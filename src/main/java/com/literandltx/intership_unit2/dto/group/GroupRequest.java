5e67735f992d625a3bb54796fe3232cdc0483295
literandltx <romanskovronn@gmail.com>
2024-05-08 16:31:31 +0300

    add group validation

--- src/main/java/com/literandltx/intership_unit2/dto/group/GroupRequest.java
+++ src/main/java/com/literandltx/intership_unit2/dto/group/GroupRequest.java
@@ -1,8 +1,10 @@
 package com.literandltx.intership_unit2.dto.group;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class GroupRequest {
+    @NotBlank
     private String title;
 }
